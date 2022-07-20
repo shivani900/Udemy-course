2541b7b143f819469e32a4d6d7951e585a32b00c
shivani feria <102295691+shivani900@users.noreply.github.com>
2022-07-20 10:36:55 +0000

    day 2

---
+++ main.py
@@ -0,0 +1,7 @@
+
+three_digit_number=int(input("enter your three_digit_number "))
+three_digit_number=str(three_digit_number)
+first_digit_number=three_digit_number[0]
+second_digit_number=three_digit_number[1]
+third_digit_number=three_digit_number[2]
+print(int(first_digit_number)+int(second_digit_number)+int(third_digit_number))
